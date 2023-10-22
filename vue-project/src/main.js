60b2ef37bbbe85ba381d68780524ea6fbb306f5b
Pimegi <pimegi@gmail.com>
2023-10-22 23:08:46 +0100

    deleted app.js, moved contents to main.js

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,28 @@
-// import './assets/main.css'
+new Vue({
+    el: '#app',
+    data: {
+      lesson: {
+        id: 1,
+        subject: "Maths",
+        location: "New York",
+        price: 100,
+        spaces: 5
+      },
+      cart: []
+    },
+    methods: {
+      addToCart(lesson) {
+        if (lesson.spaces > 0) {
+          lesson.spaces--;
+          this.cart.push(lesson.id);
+        }
+      }
+    },
 
-import { createApp } from 'vue'
-// import App from './App.vue'
-
-createApp(App).mount('#app')
+    computed:{
+        cartItemCount: function(){
+            return this.cart.length;
+        }
+    }
+  });
+  
