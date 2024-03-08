ba1f24b0199660d3425c3a51372719249caeb057
Pimegi <pimegi@gmail.com>
2024-03-08 20:25:48 +0400

    gh-pages branch commit

---
+++ lessons.js
@@ -0,0 +1,94 @@
+let lessons = [
+
+    {
+        id:1,
+        subject: "CS",
+        location: "Texas",
+        price: 1887,
+        spaces: 5,
+        icon: "fa-solid fa-graduation-cap", // Relative path to the local image
+    },
+
+    {
+        id:2,
+        subject: "Maths",
+        location: "Florida",
+        price: 482,
+        spaces: 9,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:3,
+        subject: "Law",
+        location: "Ohio",
+        price: 1248,
+        spaces: 7,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:4,
+        subject: "History",
+        location: "Alaska",
+        price: 982,
+        spaces: 8,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:5,
+        subject: "Economics",
+        location: "New Jersey",
+        price: 1064,
+        spaces: 11,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:6,
+        subject: "Language",
+        location: "New York",
+        price: 1000,
+        spaces: 15,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:7,
+        subject: "Biology",
+        location: "Arizona",
+        price: 1268,
+        spaces: 6,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:8,
+        subject: "Politics and Society",
+        location: "Alabama",
+        price: 1000,
+        spaces: 13,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:9,
+        subject: "Physics",
+        location: "Missouri",
+        price: 705,
+        spaces: 22,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+    {
+        id:10,
+        subject: "Chemistry",
+        location: "Connecticut",
+        price: 1407,
+        spaces: 19,
+        icon: "fa-solid fa-graduation-cap",
+    },
+
+];
+
